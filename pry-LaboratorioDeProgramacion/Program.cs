efc085a1a8c3cf3a58130dc6415ab0a5f27b6aa2
IvoSchwartz <ivoschwartz2005@gmail.com>
2025-03-31 12:22:08 -0300

    Agregar archivos de proyecto.

---
+++ pry-LaboratorioDeProgramacion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pry_LaboratorioDeProgramacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
